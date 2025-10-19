0b975fd1bbad079565d80090668e0aad3d8bd05b
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-19 22:54:12 +0530

    refactor(model): update size and character limits in Book entity fields

--- src/main/java/in/vyashivam/librarymanager/model/Book.java
+++ src/main/java/in/vyashivam/librarymanager/model/Book.java
@@ -26,19 +26,19 @@ public class Book {
     @Column(name = "Book ID")
     private Long bookId;
 
-    @Column(name = "Title", nullable = false, length = 75)
-    @NotBlank(message = "Title cannot be null. It must be upto 75 characters.")
-    @Size(min = 10, max = 75)
+    @Column(name = "Title", nullable = false, length = 100)
+    @NotBlank(message = "Title cannot be null. It must be between 1 to 100 characters.")
+    @Size(min = 1, max = 100)
     private String title;
 
     @Column(name = "Author", nullable = false, length = 50)
-    @NotBlank(message = "Title cannot be null. It must be upto 50 characters.")
-    @Size(min = 10, max = 50)
+    @NotBlank(message = "Name of the author cannot be null. It must be between 1 to 50 characters.")
+    @Size(min = 1, max = 50)
     private String author;
 
     @Column(name = "ISBN", nullable = false)
     @NotBlank(message = "ISBN cannot be empty.")
-    @Pattern(regexp = "^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\\\\d-]+$", message = "Enter proper ISBN of the book.") // referred online
+    @Pattern(regexp = "^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\\d-]+$", message = "Enter proper ISBN of the book.") // referred online
     private String isbn;
 
     @Column(name = "Genre", nullable = false)
