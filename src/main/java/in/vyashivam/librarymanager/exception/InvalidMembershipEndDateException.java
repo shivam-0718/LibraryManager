70c64efa2bfddc189b375178ac86c69b33645154
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-20 21:48:43 +0530

    feat(exception): add InvalidMembershipEndDateException for handling end date cases

---
+++ src/main/java/in/vyashivam/librarymanager/exception/InvalidMembershipEndDateException.java
@@ -0,0 +1,7 @@
+package in.vyashivam.librarymanager.exception;
+
+public class InvalidMembershipEndDateException extends RuntimeException {
+    public InvalidMembershipEndDateException(String message) {
+        super(message);
+    }
+}
