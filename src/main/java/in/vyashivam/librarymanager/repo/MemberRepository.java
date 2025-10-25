b875fb8e9702f2cfc25608e8f832e7f2b6e839f9
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-25 23:05:52 +0530

    feat(repository): add finder methods in MemberRepository for searching by name and emailId

--- src/main/java/in/vyashivam/librarymanager/repo/MemberRepository.java
+++ src/main/java/in/vyashivam/librarymanager/repo/MemberRepository.java
@@ -3,7 +3,15 @@ package in.vyashivam.librarymanager.repo;
 import in.vyashivam.librarymanager.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // Returning list of members (if found with same name)
+    List<Member> findByMemberNameContainingIgnoreCase(String name);
+
+    // finding a member by email id
+    Optional<Member> findByEmailId(String emailId);
+
 }
