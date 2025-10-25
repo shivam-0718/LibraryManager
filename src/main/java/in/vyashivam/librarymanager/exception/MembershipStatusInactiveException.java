798ba9031414f713ffcb1e6579bd89b1b9f0a6ad
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-25 23:05:34 +0530

    feat(exception): add MembershipStatusInactiveException for handling inactive or expiring memberships

---
+++ src/main/java/in/vyashivam/librarymanager/exception/MembershipStatusInactiveException.java
@@ -0,0 +1,7 @@
+package in.vyashivam.librarymanager.exception;
+
+public class MembershipStatusInactiveException extends RuntimeException {
+    public MembershipStatusInactiveException(String message) {
+        super(message);
+    }
+}
