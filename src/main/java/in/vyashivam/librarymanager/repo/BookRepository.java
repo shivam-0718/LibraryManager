94b777d8d5b7c7c9db19297539e946d10a075f89
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-25 23:09:38 +0530

    feat(repository): add finder methods in BookRepository for title, author, and ISBN search

--- src/main/java/in/vyashivam/librarymanager/repo/BookRepository.java
+++ src/main/java/in/vyashivam/librarymanager/repo/BookRepository.java
@@ -3,7 +3,17 @@ package in.vyashivam.librarymanager.repo;
 import in.vyashivam.librarymanager.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    // Returning list of books containing title.
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    // Returning list of books containing author name.
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+
+    // finding a book by Isbn
+    Optional<Book> findByIsbn(String isbn);
 }
