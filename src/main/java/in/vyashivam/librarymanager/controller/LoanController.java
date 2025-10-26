ab6a2d6391a09dc2a47f814182bee1a96816995a
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-26 14:54:49 +0530

    feat(controller): add getAllLoans() and returnABook() endpoints in LoanController

--- src/main/java/in/vyashivam/librarymanager/controller/LoanController.java
+++ src/main/java/in/vyashivam/librarymanager/controller/LoanController.java
@@ -3,9 +3,7 @@ package in.vyashivam.librarymanager.controller;
 import in.vyashivam.librarymanager.model.Book;
 import in.vyashivam.librarymanager.model.Loan;
 import in.vyashivam.librarymanager.model.Member;
-import in.vyashivam.librarymanager.service.IBookService;
 import in.vyashivam.librarymanager.service.ILoanService;
-import in.vyashivam.librarymanager.service.IMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/loan")
@@ -22,6 +21,12 @@ public class LoanController {
     @Autowired
     public void setLoanService(ILoanService loanService) {
         this.loanService = loanService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Loan>> getAllLoans() {
+        List<Loan> loansList = loanService.getAllLoans();
+        return new ResponseEntity<>(loansList, HttpStatus.OK);
     }
 
     @GetMapping("/rent-a-book/{memberId}/{bookId}")
@@ -46,5 +51,32 @@ public class LoanController {
                 loan.getIssueDate(), loan.getDueDate());
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/return-a-book/{loanId}")
+    public ResponseEntity<String> returnABook(@PathVariable Long loanId) {
+        Loan completedLoan = loanService.returnABook(loanId);
+        String response = String.format("""
+                Loan has been completed and the
+                book has been returned with the
+                details as follows:
+                
+                Name: %s
+                MemberId: %s
+                Loan Id: %s
+                Book Title: %s
+                Issue Date: %s
+                Return Date: %s
+                Due Date: %s
+                Fine Amount: %s
+                Total Amount: %s
+                
+                Thank you! Please visit again!
+                """, completedLoan.getMember().getMemberName(), completedLoan.getMember().getMemberId(),
+                loanId, completedLoan.getBook().getTitle(), completedLoan.getIssueDate(),
+                completedLoan.getReturnDate(), completedLoan.getDueDate(), completedLoan.getFineAmount(),
+                completedLoan.getTotalAmount());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
