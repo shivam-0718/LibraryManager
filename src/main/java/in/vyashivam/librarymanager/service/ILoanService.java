44e0153c772c588ebaddd654cb3d9871fb01b74a
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-26 14:54:21 +0530

    feat(service): add getAllLoans() and returnABook() method definitions in ILoanService

--- src/main/java/in/vyashivam/librarymanager/service/ILoanService.java
+++ src/main/java/in/vyashivam/librarymanager/service/ILoanService.java
@@ -1,7 +1,10 @@
 package in.vyashivam.librarymanager.service;
 
 import in.vyashivam.librarymanager.model.Loan;
+import java.util.List;
 
 public interface ILoanService {
+    List<Loan> getAllLoans();
     Loan borrowABook(Long memberId, Long bookId);
+    Loan returnABook(Long loanId);
 }
